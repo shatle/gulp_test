e1f4b3e9e257d33e9276c12110c2dc28fb2db250
shatle <shatle@qq.com>
2015-03-05 00:27:20 +0800

    only compile app.scss, add nanoDropdown direcitve in app.coffee

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -1,4 +1,4 @@
-define(['angular', 'angular-couch-potato', 'angular-ui-router', 'angular-ui-bootstrap', 'nanoscroller'], function(angular, couchPotato) {
+define(['angular', 'angular-couch-potato', 'angular-ui-router', 'angular-ui-bootstrap'], function(angular, couchPotato) {
   var app;
   app = angular.module('app', ['scs.couch-potato', 'ui.router']);
   couchPotato.configureApp(app);
@@ -30,29 +30,35 @@ define(['angular', 'angular-couch-potato', 'angular-ui-router', 'angular-ui-boot
   ]);
   app.controller('messagesCtrl', [
     '$scope', '$state', '$timeout', function($scope, $state, $timeout) {
-      console.log('messagesCtrl');
-      $scope.status = {
-        isopen: false
-      };
-      $scope.data = {
-        nanoContentHeight: 265
-      };
-      $scope.toggleDropdown = function($event) {
-        console.log('toggleDropdown');
-        $event.preventDefault();
-        $event.stopPropagation();
-        $scope.status.isopen = !$scope.status.isopen;
-        return $timeout(function() {
-          if ($scope.status.isopen) {
-            return $($event.target).closest('li.dropdown').find('.nano').nanoScroller({
-              scroll: 'top'
+      return console.log('messagesCtrl');
+    }
+  ]);
+  app.directive('nanoDropdown', [
+    '$timeout', function($timeout) {
+      return {
+        link: function(scope, element, attrs) {
+          console.log('nanoDropdown2222');
+          scope.status = {
+            isopen: false
+          };
+          scope.data = {
+            nanoContentHeight: 265
+          };
+          return scope.toggleDropdown = function($event) {
+            console.log('toggleDropdown222', element);
+            $event.preventDefault();
+            $event.stopPropagation();
+            scope.status.isopen = !scope.status.isopen;
+            return $timeout(function() {
+              if (scope.status.isopen) {
+                return angular.element(element).find('.nano').nanoScroller({
+                  scroll: 'top'
+                });
+              }
             });
-          }
-        });
+          };
+        }
       };
-      return $timeout(function() {
-        return console.log('nanoScroller');
-      });
     }
   ]);
   app.run([
